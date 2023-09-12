20106b701dddd9953483dc94e9841cdee2a0d307
master-dhanesh <dhanesh1296@gmail.com>
2023-09-12 18:24:12 +0530

    home change

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <div className="w-75">
+            <h3>Homepage</h3>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Quaerat est eius mollitia quos repellat in quas corporis atque
+                dicta, quam dolores commodi cumque reprehenderit illo illum
+                dignissimos voluptatum laboriosam omnis. Delectus expedita, ea
+                iusto enim suscipit nobis ad maiores magnam quidem sint illum
+                cum doloremque iste laboriosam iure aperiam id! Nemo explicabo
+                cum, minima doloribus quidem, quasi esse dicta sunt, quod
+                suscipit praesentium rerum blanditiis iste dolores cumque
+                tempora sint nobis illum sequi aut maxime aliquam ullam
+                corrupti. Harum possimus assumenda ratione nemo suscipit
+                delectus consequuntur reprehenderit natus illo nulla esse cumque
+                saepe numquam, adipisci accusantium dolorum distinctio
+                provident! Quidem.
+            </p>
+        </div>
+    );
 };
 
 export default Home;
